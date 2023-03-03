1adc2667425b52ec67325511f866c47b4ce600d7
hyeseo <shmyth4@gmaii.com>
2023-03-03 13:11:01 +0900

    feat - 회원 도메인 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 추상화, 구현 모두 의존(DIP 위반)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
