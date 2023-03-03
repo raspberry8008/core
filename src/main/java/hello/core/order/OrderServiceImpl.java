38de3bb4e403eed4712c8e8726624e0f0f877095
hyeseo <shmyth4@gmaii.com>
2023-03-03 14:56:09 +0900

    feat - 주문 도메인 구현

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPoicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPoicy();
+
+    /**
+     * 주문생성 기능
+     * 1.회원정보 조회
+     * 2.할인 정책 적용
+     * @return Order Object
+     */
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
